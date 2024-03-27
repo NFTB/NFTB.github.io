97662c592293ce82ead4a3dab16c8631b3ec975d
NFTB <2757629107@qq.com>
2024-03-27 14:45:04 +0800

    Site updated: 2024-03-27 14:45:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/14/c-多线程/"];function toRandomPost(){
+var posts=["2024/03/14/c-多线程/","2024/03/27/LRU-K-15445p1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
